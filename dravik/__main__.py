6290361793087c15bb5f57e733d7b5cae0c3eefb
Yaser Amiri <yaser.amiri95@gmail.com>
2025-02-15 13:03:16 +0100

    Fixed dravik-init command bug (wrong path)

--- dravik/__main__.py
+++ dravik/__main__.py
@@ -1,11 +1,13 @@
 import os
+from functools import partial
+import asyncio
 
 from dravik.app import Dravik
 
 
 app = Dravik(config_dir=os.environ.get("DRAVIK_DIR", "") or None)
 run_app = app.run
-init = app.services.create_configs
+init = partial(asyncio.run, app.services.create_configs())
 
 
 if __name__ == "__main__":
