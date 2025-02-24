6dc6e39e04b4d789015095e560220cff749b40ea
Yaser Amiri <yaser.amiri95@gmail.com>
2025-02-24 16:08:39 +0100

    Fixed "refresh data" command bug
    Refresh was not working because the state was not being "mutate" after
    assiging the new state to the app.

--- dravik/screens/refresh.py
+++ dravik/screens/refresh.py
@@ -3,7 +3,7 @@ from textual.containers import Horizontal, Vertical
 from textual.screen import ModalScreen
 from textual.widgets import Button, Label
 
-from dravik.utils import get_app_services, get_app_state
+from dravik.utils import get_app_services, get_app_state, mutate_app_state
 
 
 class RefreshScreen(ModalScreen[None]):
@@ -25,4 +25,5 @@ class RefreshScreen(ModalScreen[None]):
         if event.button.id == self.ns("refresh-btn"):
             state = get_app_state(self.app)
             state.ledger_data = get_app_services(self.app).read_hledger_data()
+            mutate_app_state(self.app)
         self.app.pop_screen()
